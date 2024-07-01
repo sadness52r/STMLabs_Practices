0b76e4ab4da47db039a848a224dc75b3d8c901ab
sadness52r <bukin.k2017@gmail.com>
2024-07-01 20:31:18 +0300

    add new class Calculator and create some functions

---
+++ src/Practice1_Git/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1_Git
+{
+    internal class Calculator
+    {
+        public int Summ(int a, int b) { return a + b; }
+        public int Sub(int a, int b) { return a - b; }
+        public int Mul(int a, int b) { return a * b; }
+        public double Div(int a, int b) { return (double)a / b; }
+    }
+}
