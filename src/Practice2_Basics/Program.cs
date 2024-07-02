f0709982c5bc79779ff55d97cec881403880f002
sadness52r <bukin.k2017@gmail.com>
2024-07-02 13:46:35 +0300

    complete task 3 in topic Strings

--- src/Practice2_Basics/Program.cs
+++ src/Practice2_Basics/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace Practice2_Basics
+﻿using System;
+
+namespace Practice2_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Task 1:");
             StringsTask1 task1 = new StringsTask1();
             task1.Run();
+
+            Console.WriteLine("\nTask3:");
+            StringsTask3 stringsTask3 = new StringsTask3();
+            stringsTask3.Run();
         }
     }
 }
---
+++ src/Practice2_Basics/StringsTask3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice2_Basics
+{
+    internal class StringsTask3
+    {
+        private void PrintCountInfo(string str, char symb)
+        {
+            Console.WriteLine($"Letter {symb} appears {str.Count(x => x == symb)} times");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Please, enter any string:");
+            string str = Console.ReadLine();
+            PrintCountInfo(str, 'r');
+            PrintCountInfo(str, 'k');
+            PrintCountInfo(str, 't');
+        }
+    }
+}
