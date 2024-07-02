542e2b9bd98e50649e82e20aaf2a326db8f9a5fa
sadness52r <bukin.k2017@gmail.com>
2024-07-02 13:16:05 +0300

    fix logic of methods CheckValidParsing, CalculateNumbers and Run

--- src/Practice2_Basics/Program.cs
+++ src/Practice2_Basics/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice2_Basics
+﻿namespace Practice2_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Conditions_Task5 conditions_Task5 = new Conditions_Task5();
+            ConditionsTask5 conditions_Task5 = new ConditionsTask5();
             conditions_Task5.Run();
         }
     }
