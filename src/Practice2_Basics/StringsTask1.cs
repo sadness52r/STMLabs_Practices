eba6c323cc8b9201709f4ae34bc271bb4a1d7c0a
sadness52r <bukin.k2017@gmail.com>
2024-07-02 12:40:19 +0300

    add new class StringsTask1

---
+++ src/Practice2_Basics/StringsTask1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Practice2_Basics
+{
+    internal class StringsTask1
+    {
+        private const string LETTERS_TEMPLATE = "ABCEHKMOPTXY";
+        //private bool CheckNumber(string number)
+        //{
+        //    Regex regex = new Regex(@"(ABCEHKMOPTXY){1}[0-9]{3}(ABCEHKMOPTXY){2}");
+        //    return regex.IsMatch(number);
+        //}
+        //public void Run()
+        //{
+        //    while (true)
+        //    {
+        //        string number = Console.ReadLine();
+        //        if (number.Length == 0)
+        //        {
+        //            return;
+        //        }
+
+                
+        //    }
+        //}
+    }
+}
