3b85f1da1dbafaa172859606ec37a3845f76f518
Kirill Bukin <kebukin@edu.hse.ru>
2024-07-03 23:52:14 +0300

    fix GetLogValidation method and add CheckValidation method

--- src/Practice2_Basics/StringsTask1.cs
+++ src/Practice2_Basics/StringsTask1.cs
@@ -7,34 +7,42 @@ namespace Practice2_Basics
         private const string LETTERS_TEMPLATE_ENG = "ABCEHKMOPTXY";
         private const string LETTERS_TEMPLATE_RU = "АВСЕНКМОРТХУ";
 
-        private string GetLogValidation(string number)
+        private bool CheckValidation(string number, out string errorMessage)
         {
-            if (number.Length < 6 || number.Length > 6)
+            errorMessage = null;
+            if (number.Length != 6)
             {
-                return $"{number} is invalid!\nYour number length: {number.Length}. Need: 6";
+                errorMessage = $"Your number length: {number.Length}. Need: 6";
+                return false;
             }
             if (!char.IsLetter(number[0]) || !char.IsLetter(number[4]) || !char.IsLetter(number[5]))
             {
-                return $"{number} is invalid!\n{number[0]}, {number[4]} or {number[5]} is not a letter!";
+                errorMessage = $"{number[0]}, {number[4]} or {number[5]} is not a letter!";
+                return false;
             }
             if (!char.IsDigit(number[1]) || !char.IsDigit(number[2]) || !char.IsDigit(number[3]))
             {
-                return $"{number} is invalid!\n{number[1]}, {number[2]} or {number[3]} is not a digit!";
+                errorMessage = $"{number[1]}, {number[2]} or {number[3]} is not a digit!";
+                return false;
             }
             if (LETTERS_TEMPLATE_ENG.IndexOf(number[0]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[0]) == -1
                 || LETTERS_TEMPLATE_ENG.IndexOf(number[4]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[4]) == -1
                 || LETTERS_TEMPLATE_ENG.IndexOf(number[5]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[5]) == -1)
             {
-                return $"{number} is invalid!\n{number[0]}, {number[4]} or {number[5]} is incorrect letter!";
+                errorMessage = $"{number[0]}, {number[4]} or {number[5]} is incorrect letter!";
+                return false;
             }
             if (LETTERS_TEMPLATE_ENG.IndexOf(number[0]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[0]) != -1
                 || LETTERS_TEMPLATE_ENG.IndexOf(number[4]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[4]) != -1
                 || LETTERS_TEMPLATE_ENG.IndexOf(number[5]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[5]) != -1)
             {
-                return $"{number} is invalid!\n{number[0]}, {number[4]} or {number[5]} is incorrect Russian letter! Try to replace it to the same Eng!";
+                errorMessage = $"{number[0]}, {number[4]} or {number[5]} is incorrect Russian letter! Try to replace it to the same Eng!";
+                return false;
             }
-            return $"{number} is valid!";
+            return true;
         }
+        private string GetLogValidation(string number) =>
+            CheckValidation(number, out string errorMessage) ? $"{number} is valid!" : $"{number} is invalid!\n{errorMessage}";
         public void Run()
         {
             Console.WriteLine("Please, enter bus numbers (For exit, press ENTER):");
