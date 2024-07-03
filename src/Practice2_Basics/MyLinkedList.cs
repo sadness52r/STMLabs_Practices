0fb85d43a49dff1ac810dc6021bb20c9ebbe252c
Kirill Bukin <kebukin@edu.hse.ru>
2024-07-04 01:39:48 +0300

    add class MyLinkedList and create Add and Remove functions

---
+++ src/Practice2_Basics/MyLinkedList.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Practice2_Basics
+{
+    internal class MyLinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+        public int Size { get; private set; }
+
+        public MyLinkedList()
+        {
+            Head = null;
+        }
+        public MyLinkedList(Node<T> head)
+        {
+            Head = head;
+        }
+
+        public void Add(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Size++;
+                return;
+            }
+
+            Node<T> tmpHead = Head;
+            while (tmpHead.Next != null)
+            {
+                tmpHead = tmpHead.Next;
+            }
+            tmpHead.Next = newNode;
+            Size++;
+        }
+        public void Remove(int nodeNumber)
+        {
+            if (nodeNumber < 0)
+            {
+                Console.WriteLine("This node is unvailable!");
+                return;
+            }
+            int curNode = 0;
+            Node<T> tmpHead = Head;
+            Node<T> prevNode = null;
+            while (curNode != nodeNumber)
+            {
+                prevNode = Head;
+                Head = Head.Next;
+                curNode++;
+            }
+            if (curNode == nodeNumber && Head == null)
+            {
+                Console.WriteLine("This node is unvailable!");
+                Head = tmpHead;
+                return;
+            }
+            if (prevNode == null)
+            {
+                Head = Head.Next;
+            }
+            else
+            {
+                prevNode.Next = Head.Next;
+                Head = tmpHead;
+            }
+            Size--;
+        }
+        public void Print()
+        {
+            Node<T> tmpHead = Head;
+            Console.WriteLine("Your linked list:");
+            while (Head != null)
+            {
+                Console.Write($"{Head.Value} ");
+                Head = Head.Next;
+            }
+            Head = tmpHead;
+            Console.WriteLine();
+        }
+    }
+}
--- src/Practice2_Basics/Program.cs
+++ src/Practice2_Basics/Program.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice2_Basics
+﻿namespace Practice2_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            MyLinkedList<int> myLinkedList = new MyLinkedList<int>();
+            myLinkedList.Print();
+            myLinkedList.Add(1);
+            myLinkedList.Print();
+            myLinkedList.Add(2);
+            myLinkedList.Print();
+            myLinkedList.Add(3);
+            myLinkedList.Print();
+            myLinkedList.Add(4);
+            myLinkedList.Print();
+            myLinkedList.Remove(3);
+            myLinkedList.Print();
+            myLinkedList.Remove(1);
+            myLinkedList.Print();
+            myLinkedList.Remove(2);
+            myLinkedList.Print();
+            myLinkedList.Add(5);
+            myLinkedList.Print();
+            myLinkedList.Remove(0);
+            myLinkedList.Print();
         }
     }
 }
