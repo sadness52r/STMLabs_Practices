2f6b3266c19b0ae50e2cc52cc26e92034633f3d1
Kirill Bukin <kebukin@edu.hse.ru>
2024-07-03 13:09:34 +0300

    fix and upgrade functions CountFrequencies and GetLogValidation

--- src/Practice2_Basics/StringsTask1.cs
+++ src/Practice2_Basics/StringsTask1.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 
 namespace Practice2_Basics
 {
     internal class StringsTask1
     {
-        private const string LETTERS_TEMPLATE = "ABCEHKMOPTXY";
+        private const string LETTERS_TEMPLATE_ENG = "ABCEHKMOPTXY";
+        private const string LETTERS_TEMPLATE_RU = "АВСЕНКМОРТХУ";
 
-        private bool CheckNumber(string number)
+        private string GetLogValidation(string number)
         {
-            return new Regex($"[{LETTERS_TEMPLATE}]{{1}}[0-9]{{3}}[{LETTERS_TEMPLATE}]{{2}}").IsMatch(number);
+            if (number.Length < 6 || number.Length > 6)
+            {
+                return $"{number} is invalid!\nYour number length: {number.Length}. Need: 6";
+            }
+            if (!char.IsLetter(number[0]) || !char.IsLetter(number[4]) || !char.IsLetter(number[5]))
+            {
+                return $"{number} is invalid!\n{number[0]}, {number[4]} or {number[5]} is not a letter!";
+            }
+            if (!char.IsDigit(number[1]) || !char.IsDigit(number[2]) || !char.IsDigit(number[3]))
+            {
+                return $"{number} is invalid!\n{number[1]}, {number[2]} or {number[3]} is not a digit!";
+            }
+            if (LETTERS_TEMPLATE_ENG.IndexOf(number[0]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[0]) == -1
+                || LETTERS_TEMPLATE_ENG.IndexOf(number[4]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[4]) == -1
+                || LETTERS_TEMPLATE_ENG.IndexOf(number[5]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[5]) == -1)
+            {
+                return $"{number} is invalid!\n{number[0]}, {number[4]} or {number[5]} is incorrect letter!";
+            }
+            if (LETTERS_TEMPLATE_ENG.IndexOf(number[0]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[0]) != -1
+                || LETTERS_TEMPLATE_ENG.IndexOf(number[4]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[4]) != -1
+                || LETTERS_TEMPLATE_ENG.IndexOf(number[5]) == -1 && LETTERS_TEMPLATE_RU.IndexOf(number[5]) != -1)
+            {
+                return $"{number} is invalid!\n{number[0]}, {number[4]} or {number[5]} is incorrect Russian letter! Try to replace it to the same Eng!";
+            }
+            return $"{number} is valid!";
         }
         public void Run()
         {
@@ -21,15 +45,7 @@ namespace Practice2_Basics
                 {
                     return;
                 }
-
-                if (CheckNumber(number))
-                {
-                    Console.WriteLine($"{number} - is valid!");
-                }
-                else
-                {
-                    Console.WriteLine($"{number} - is not valid!");
-                }
+                Console.WriteLine(GetLogValidation(number));
             }
         }
     }
--- src/Practice2_Basics/StringsTask3.cs
+++ src/Practice2_Basics/StringsTask3.cs
@@ -1,21 +1,40 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Practice2_Basics
 {
     internal class StringsTask3
     {
-        private void PrintCountInfo(string str, char symb)
+        private Dictionary<char, int> frequencies;
+
+        public StringsTask3()
         {
-            Console.WriteLine($"Letter {symb} appears {str.Count(x => x == symb)} times");
+            frequencies = new Dictionary<char, int>();
+        }
+
+        private void CountFrequencies(string str)
+        {
+            foreach (char item in str)
+            {
+                if (item == 'r' || item == 'k' || item == 't')
+                {
+                    if (!frequencies.ContainsKey(item))
+                    {
+                        frequencies[item] = 1;
+                        continue;
+                    }
+                    frequencies[item]++;
+                }
+            }
         }
         public void Run()
         {
             Console.WriteLine("Please, enter any string:");
             string str = Console.ReadLine();
-            PrintCountInfo(str, 'r');
-            PrintCountInfo(str, 'k');
-            PrintCountInfo(str, 't');
+            CountFrequencies(str);
+            Console.WriteLine($"Letter r appears {(frequencies.ContainsKey('r') ? frequencies['r'] : 0)} times");
+            Console.WriteLine($"Letter k appears {(frequencies.ContainsKey('k') ? frequencies['k'] : 0)} times");
+            Console.WriteLine($"Letter t appears {(frequencies.ContainsKey('t') ? frequencies['t'] : 0)} times");
         }
     }
 }
