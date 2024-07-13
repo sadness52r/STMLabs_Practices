c3491f862ee7fa785de8f75759ba9a9c11d30272
Kirill Bukin <kebukin@edu.hse.ru>
2024-07-13 19:30:37 +0300

    complete task 2 in topic "Delegates"

--- src/Practice3_DelegatesEventsExceptions/Program.cs
+++ src/Practice3_DelegatesEventsExceptions/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Practice3_DelegatesEventsExceptions
+﻿using System;
+
+namespace Practice3_DelegatesEventsExceptions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            DaysOfWeekTask2 task = new DaysOfWeekTask2();
+            while (true)
+            {
+                Console.WriteLine("Click ENTER to call a delegate!\nExit: enter something else");
+                if (Console.ReadLine() == String.Empty)
+                {
+                    Console.WriteLine(task.CallDelegate()); 
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
