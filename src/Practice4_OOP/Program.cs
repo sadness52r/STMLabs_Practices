95fe26c7a0770486b5818e34942ccd9732f50259
sadness52r <bukin.k2017@gmail.com>
2024-07-18 14:31:40 +0300

    create new project for practice 4

---
+++ src/Practice4_OOP/Program.cs
@@ -0,0 +1,15 @@
+namespace Practice4_OOP
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
