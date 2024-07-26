29edfec14e49f0cb852fa23b2bba1d5f6f8a058a
sadness52r <bukin.k2017@gmail.com>
2024-07-26 12:40:17 +0300

    start to do practice 5 "Reflection"

---
+++ src/Practice5_MySerializer/MySerializer.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice5_MySerializer
+{
+    public class MySerializer
+    {
+        private Type type;
+
+        public MySerializer(Type type)
+        {
+            //this.type = type;
+            //if (!type.IsDefined(typeof(SerializeAttribute), false))
+            //{
+            //    throw new ArgumentException("The type has not necessary attribute!");
+            //}
+        }
+    }
+}
