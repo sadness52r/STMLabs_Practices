50082ad4ec59198a04ee2c4004af15968c5aa98a
sadness52r <bukin.k2017@gmail.com>
2024-07-26 13:44:38 +0300

    Merged Task 5 and Practice 3

---
+++ src/Practice3_DelegatesEventsException/CustomExceptions/WrongStartingSymbolException.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice3_DelegatesEventsExceptions
+{
+    internal class WrongStartingSymbolException : IncorrectStringException
+    {
+        private readonly char symbol;
+
+        public WrongStartingSymbolException(int _lineNumber, char symbol) :
+            base(_lineNumber, symbol)
+        {
+            this.symbol = symbol;
+        }
+    }
+}
--- src/Practice3_DelegatesEventsException/Program.cs
+++ src/Practice3_DelegatesEventsException/Program.cs
@@ -4,6 +4,8 @@ using Practice3_DelegatesEventsExceptions;
 
 internal class Program
 {
+    private const string FILENAME = "data.txt";
+
     private static void Main(string[] args)
     {
         ILogger loggerError = LoggerFactory.Create(builder =>
@@ -99,5 +101,56 @@ internal class Program
         sourceTask4_1.Notify += handlerTask4.GetArgValue;
         sourceTask4_2.Notify += handlerTask4.GetArgValue;
         sourceTask4_1.GenerateEvent(); sourceTask4_2.GenerateEvent();
+
+        Console.WriteLine("Enter prohibited and erroneous templates");
+        string prohibited = Console.ReadLine();
+        string erroneous = Console.ReadLine();
+        int proLimit;
+        while (true)
+        {
+            Console.WriteLine("Enter proLimit parameter");
+            try
+            {
+                proLimit = int.Parse(Console.ReadLine());
+                break;
+            }
+            catch (FormatException e)
+            {
+                loggerError.LogError(e, e.Message);
+            }
+        }
+        try
+        {
+            LimitedStringLoader limitedStringLoader = new LimitedStringLoader(prohibited, erroneous, proLimit);
+            limitedStringLoader.Load(FILENAME);
+            foreach (var item in limitedStringLoader.ParsedStrings)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        catch (InconsistentLimitsException e)
+        {
+            loggerError.LogError(e, e.Message);
+        }
+        catch (TooManyProhibitedLinesException e)
+        {
+            loggerError.LogError(e, e.Message);
+        }
+        catch (WrongStartingSymbolException e)
+        {
+            loggerError.LogError(e, e.Message);
+        }
+        catch (IncorrectStringException e)
+        {
+            loggerError.LogError(e, e.Message);
+        }
+        catch (DataNotLoadedException e)
+        {
+            loggerError.LogError(e, e.Message);
+        }
+        catch (FileNotFoundException e)
+        {
+            loggerError.LogError(e, e.Message);
+        }
     }
 }
